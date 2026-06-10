8031457fa798ba4605132b288c8a9d52dfb3c475
Yair Peleg <ptyair101@gmail.com>
2026-06-10 17:42:50 +0300

    game maneger extend terrain and add avatar

--- pepse/PepseGameManager.java
+++ pepse/PepseGameManager.java
@@ -6,15 +6,20 @@ import danogl.gui.ImageReader;
 import danogl.gui.SoundReader;
 import danogl.gui.UserInputListener;
 import danogl.gui.WindowController;
+import danogl.gui.rendering.Camera;
 import danogl.util.Vector2;
 import pepse.world.Block;
 import pepse.world.Sky;
 import pepse.world.Terrain;
+import pepse.world.avatar.Avatar;
 
 
 import java.util.List;
 
 public class PepseGameManager extends GameManager {
+    private static final int SEED = 1;
+    private static final float AVATAR_SIZE = 50;
+
     public static void main(String[] args) {
         new PepseGameManager().run();
     }
@@ -30,13 +35,18 @@ public class PepseGameManager extends GameManager {
         int skyLayer = Layer.BACKGROUND;
         gameObjects().addGameObject(sky, skyLayer);
 
-        Terrain t = new Terrain(windowDimensions, 1);
-        List<Block> tb =  t.createInRange(0, (int) windowDimensions.x());
+        Terrain t = new Terrain(windowDimensions, SEED);
+        List<Block> tb =  t.createInRange((int) -windowDimensions.x(), (int) (2 * windowDimensions.x()));
         for (Block b : tb){
             gameObjects().addGameObject(b, Layer.STATIC_OBJECTS);
         }
 
+        float avatarX = windowDimensions.x() / 2;
+        float avatarY = t.groundHeightAt(avatarX) - AVATAR_SIZE;
+        Avatar avatar = new Avatar(new Vector2(avatarX, avatarY), inputListener, imageReader);
+        gameObjects().addGameObject(avatar, Layer.DEFAULT);
 
+        setCamera(new Camera(avatar, Vector2.ZERO, windowDimensions, windowDimensions));
 
     }
 }
