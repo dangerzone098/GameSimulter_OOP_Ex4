492a797081232ec217172f3671e58b2c6089c61e
Yair Peleg <ptyair101@gmail.com>
2026-06-10 17:26:41 +0300

    com2

--- pepse/world/avatar/AvatarState.java
+++ pepse/world/avatar/AvatarState.java
@@ -1,6 +1,9 @@
 package pepse.world.avatar;
 
 public interface AvatarState {
-    
-    
+    void enter(Avatar avatar);
+
+    void update(Avatar avatar, float deltaTime);
+
+    void exit(Avatar avatar);
 }
--- pepse/world/avatar/IdleState.java
+++ pepse/world/avatar/IdleState.java
@@ -1,5 +1,18 @@
 package pepse.world.avatar;
 
 public class IdleState implements AvatarState {
-    
+    @Override
+    public void enter(Avatar avatar) {
+        avatar.setIdleAnimation();
+        avatar.stopHorizontalMovement();
+    }
+
+    @Override
+    public void update(Avatar avatar, float deltaTime) {
+        avatar.addEnergy(Avatar.IDLE_ENERGY_GAIN);
+    }
+
+    @Override
+    public void exit(Avatar avatar) {
+    }
 }
